0d8202651f6e2d02a78148b978938b41b2f2ca74
Chathan Driehuys <chathan@ultimanager.com>
2019-09-24 19:37:18 -0400

    Add code coverage for tests.

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}"],
   preset: "ts-jest",
   setupFilesAfterEnv: ["@testing-library/react/cleanup-after-each"]
 };
