c37b826edf03fe97a2aac674d7bb9e381e7928bd
Chathan Driehuys <chathan@driehuys.com>
2019-09-26 20:50:32 -0400

    Tweak form input styles.

--- src/components/forms/InputLabel.ts
+++ src/components/forms/InputLabel.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 /** A lable for a form field. */
 const InputLabel = styled.label`
+  color: #333;
   display: block;
   font-family: "Open Sans", sans-serif;
 `;
--- src/components/forms/TextInput.ts
+++ src/components/forms/TextInput.ts
@@ -2,10 +2,18 @@ import styled from "styled-components";
 
 /** An input field for any kind of alphanumeric data. */
 const TextInput = styled.input`
+  border: 1px solid #aaa;
+  border-radius: 3px;
   box-sizing: border-box;
   display: block;
   font-family: "Open Sans", sans-serif;
+  margin: 0.5em 0;
+  padding: 0.5em;
   width: 100%;
+
+  &:focus {
+    box-shadow: 0 0 2px 2px lightskyblue;
+  }
 `;
 
 export default TextInput;
